07ace4557194f054638504d103a6a64564c2a114
Claudio Lucas <claudiolucas19@hotmail.com>
2023-05-08 19:46:30 -0300

    brincando com desestrutração de objetos no react

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,25 @@
 import { useState } from "react";
+import { users } from "./components/testes";
 
 import "./App.css";
+import DigaMeuNome from "./components/DigaMeuNome";
 
 function App() {
-	const [todoItem, setTodoItem] = useState("");
-	const [todoList, setTodoList] = useState([]);
-	const [id, setId] = useState(0);
-
-	function addTodoItem(props, item) {
-		let objTodo = {
-			id: id,
-			conteudo: todoItem
-		};
-		if (objTodo.conteudo !== "") {
-			setTodoList([...todoList, objTodo]);
-		} 
-
-		setTodoItem("");
-		console.log(todoList);
-		// setTodoList([...todoList, objTodo])
-		setId(id + 1);
-	}
-	const handleDeleteTodo = (id) => {
-		setTodoList(todoList.filter((item) => item.id !== id));
-		console.log(todoList);
-	};
-
-	const handleUpdateTodo = (id) => {
-		
-		const currentTodoIndex = todoList.findIndex((todo) => todo.id === id);
-		const updatedTodo = {
-			...todoList[currentTodoIndex],
-			conteudo: todoItem
-		};
-		const newTodos = [...todoList];
-		newTodos[currentTodoIndex] = updatedTodo;
-		setTodoList(newTodos);
-	};
-
 	return (
-		<div className="App">
-			<input
-				type="text"
-				value={todoItem}
-				onChange={(e) => setTodoItem(e.target.value)}
-			/>
-			<button onClick={addTodoItem}>Enviar</button>
-
-			{todoList.map((todo) => (
-				
-				<div key={todo.id}>
-					<p style={{ color: "red" }}>{todo.conteudo}</p>
-					<button onClick={() => handleDeleteTodo(todo.id)}>
-						delete
-					</button>
-					<button onClick={() => handleUpdateTodo(todo.id)}>
-						Editar Todo
-					</button>
-				</div>
-			))}
-		</div>
+		<>
+			<DigaMeuNome nome="Claudio" idade="28" />
+			<div>
+				{users.map((user) =>
+					(({ id, name, email, address: { street, city, zipcode , geo:{lat,lng} } }) => (
+						<ul key={id}>
+							<li >
+								{name} - {email} - {`rua`} {street} - {`cidade`} {city} - {`coordenadas`}: {lat}, {lng} - {`zip`}- {zipcode}
+							</li>
+						</ul>
+					))(user)
+				)}
+			</div>
+		</>
 	);
 }
 
---
+++ src/components/DigaMeuNome.jsx
@@ -0,0 +1,9 @@
+const DigaMeuNome = ({nome, idade}) => {
+  return (
+    <div>
+        <h1>{`parabéns ${nome}, \n por ter feito  ${idade} anos`}</h1>
+    </div>
+  )
+}
+
+export default DigaMeuNome
